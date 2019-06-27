f8b16fb9c0f216a2e12dfd0f9d510fb1ac38d859
杨俊宁 <1003719811@qq.com>
2019-06-28 00:59:33 +0800

    SsRating

--- demo/src/components/SsRating.tsx
+++ demo/src/components/SsRating.tsx
@@ -1,38 +1,19 @@
+/* eslint-disable react/no-multi-comp */
 import React, { Component } from 'react'
-import { DatePicker, List } from '@sishuguojixuefu/antd-mobile-rn'
 import { View } from 'react-native'
-import { Rating, AirbnbRating } from 'react-native-ratings'
-import PropTypes from 'prop-types'
+import { AirbnbRating } from 'react-native-ratings'
 import { RatingProps } from '../utils/PropTypes'
 import ErrorTip from './helper/ErrorTip'
 import getFieldDecorator from '../utils/getFieldDecorator'
 import Label from './helper/Label'
 
-export class SsRatingView extends Component {
-  static propTypes = {
-    onChange: PropTypes.func,
-    label: PropTypes.string,
-    required: PropTypes.bool,
-    defaultValue: PropTypes.number,
-  }
-
-  constructor(props) {
-    super(props)
-    const { defaultValue } = props
-    this.state = {
-      value: defaultValue || 5,
-    }
-  }
-
+export class SsRatingView extends Component<any, any> {
   private ratingCompleted = (nValue: number) => {
-    this.setState({
-      value: nValue,
-    })
     const { onChange } = this.props
     onChange(nValue)
   }
 
-  render() {
+  public render() {
     const { label, required, defaultValue } = this.props
     return (
       <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingRight: 10, paddingVertical: 8 }}>
@@ -60,16 +41,13 @@ export default class SsRating extends Component<RatingProps, {}> {
     this.fieldDecorator = getFieldDecorator(form, id, defaultValue, required, rules)
   }
 
-  private ratingCompleted = (value: number) => {
-    const { onChange } = this.props
-    onChange(value)
-  }
-
   public render() {
     const { label, required, form, id, onChange, defaultValue } = this.props
     return (
       <ErrorTip error={form.getFieldError(id)}>
-        {this.fieldDecorator(<SsRatingView label={label} required onChange={onChange} defaultValue={defaultValue} />)}
+        {this.fieldDecorator(
+          <SsRatingView label={label} required={required} onChange={onChange} defaultValue={defaultValue} />
+        )}
       </ErrorTip>
     )
   }
