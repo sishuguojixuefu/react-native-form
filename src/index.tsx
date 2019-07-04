05ef94c8ca629b2e19d005e954246a61ebee13a7
杨俊宁 <1003719811@qq.com>
2019-07-04 16:23:32 +0800

    不向外暴露ErrorTip

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import Rules from './utils/Rules'
 import Form from './Form'
-import ErrorTip from './components/helper/ErrorTip'
 
-export { Rules, ErrorTip }
+export { Rules }
 export * from './components'
 export default Form
