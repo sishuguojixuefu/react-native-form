89b27c358dd579f68b6991011c321cd566bb8243
杨俊宁 <1003719811@qq.com>
2019-07-05 17:54:17 +0800

    给 SSSelect 加 Provider

--- src/components/SsSelect.tsx
+++ src/components/SsSelect.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Picker, List } from '@sishuguojixuefu/antd-mobile-rn'
+import { Picker, List, Provider } from '@sishuguojixuefu/antd-mobile-rn'
 import omit from 'lodash.omit'
 import { SsSelectPropsType } from '../utils/PropTypes'
 import ErrorTip from './helper/ErrorTip'
@@ -41,21 +41,23 @@ export default class SsSelect extends Component<SsSelectPropsType, {}> {
     const { placeholder, label, required, form, id } = this.props
     const omitDefaultValueProps = omit(this.props, ['defaultValue'])
     return (
-      <ErrorTip error={form.getFieldError(id)}>
-        {this.fieldDecorator(
-          <Picker
-            {...omitDefaultValueProps}
-            cols={1}
-            onChange={this._onChange}
-            extra={placeholder}
-            data={this._getData()}
-          >
-            <List.Item arrow="horizontal" style={{ paddingLeft: 0 }} last>
-              <Label required={required} label={label} />
-            </List.Item>
-          </Picker>
-        )}
-      </ErrorTip>
+      <Provider>
+        <ErrorTip error={form.getFieldError(id)}>
+          {this.fieldDecorator(
+            <Picker
+              {...omitDefaultValueProps}
+              cols={1}
+              onChange={this._onChange}
+              extra={placeholder}
+              data={this._getData()}
+            >
+              <List.Item arrow="horizontal" style={{ paddingLeft: 0 }} last>
+                <Label required={required} label={label} />
+              </List.Item>
+            </Picker>
+          )}
+        </ErrorTip>
+      </Provider>
     )
   }
 }
