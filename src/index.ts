d02f058ec6584543528df5d74498c99f2c6b387c
杨俊宁 <1003719811@qq.com>
2019-07-24 15:25:47 +0800

    添加 PickerPropTypes

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,9 @@ import Form from './Form'
 
 export { Rules }
 export * from './components'
+export interface PickerPropTypes {
+  label: string
+  value: (string | undefined)[]
+}
 
 export default Form
